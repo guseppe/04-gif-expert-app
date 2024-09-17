797cabe97f475d5a73888eef89abb6020025e1d3
guseppe <yucepi@gmail.com>
2024-09-17 00:28:45 -0400

    Implementando isLoading basico

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -11,10 +11,13 @@ export const GifGrid = ({category}) => {
   //   getImages();
   // }, [] );
   const {images, isLoading} = useFetchGifs(category);
-  console.log(isLoading);
+  
   return (
     <>
       <h3>{category}</h3>
+      {
+         isLoading && <h2>Cargando...</h2>
+      }
       <div className="card-grid">
         {
           images.map( image =>(

5ff4244afc73cc9a8cbe9304fc07d225e96e2392
guseppe <yucepi@gmail.com>
2024-10-06 14:47:02 -0400

    progreso de gifexpertapp tests

--- tests/GifExpertApp.test.jsx
+++ tests/GifExpertApp.test.jsx
@@ -1,14 +1,33 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import {GifExpertApp} from '../src/GifExpertApp'
+
 describe('tests in <GifExpertApp/>', () => { 
-    test('should first', () => { 
+    const inputValue = "dragon ball";
+    test('should add category to the grid', () => { 
        render(<GifExpertApp/>);
-       screen.debug();
-       //tarea:
-       //evaluar la funcionalidad onaddcategory 
-       //que pasaria si vuelvo a mandar la misma categoria, que pasaria si mando una diferente
-       //que pasa si escribimos en el input
-       //que pasa si le damos submit al form
-    
-    });    
+       const input = screen.getByRole('textbox');
+       const form = screen.getByRole('form');
+       fireEvent.input(input, {target: {value: inputValue}} );
+       fireEvent.submit(form);
+       //screen.debug();
+       expect(screen.getByText(inputValue)).toBeTruthy();
+    });   
+    test('should not add category if already exists', () => { 
+        render(<GifExpertApp/>);
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+        fireEvent.input(input, {target: {value: inputValue}} );
+        fireEvent.submit(form);
+        fireEvent.input(input, {target: {value: inputValue}} );
+        fireEvent.submit(form);
+        expect(screen.getAllByText(inputValue).length).not.toBeGreaterThan(1);
+        //screen.debug();
+    }); 
+    test('should find images on load', async() => { 
+        render(<GifExpertApp/>);
+        // setTimeout(() => {
+        //     screen.debug();
+        // }, 5000);
+       //como esperar a que carguen las imagenes, mas adelante se vera como se haceq
+    });
 })
